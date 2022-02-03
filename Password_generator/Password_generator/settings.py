1e76f00bf347dd71a1777f211f6bb1fc06a4b047
Tyler <tylerlangsam@icloud.com>
2022-02-03 22:39:14 +0000

    Update Settings

--- Password_generator/Password_generator/settings.py
+++ Password_generator/Password_generator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f-jooj2t3sdj!sm-^(_%kjapb$p0iq+pw!8)^(4m6r3$)q@a72'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Tylerlangsam.pythonanywhere.com']
 
 
 # Application definition
